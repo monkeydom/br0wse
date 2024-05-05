cb4f529163f54ccd47f0e03f8111994f08c37f25
Dominik Wagner <wagnr@icloud.com>
2024-05-05 14:48:48 +0200

    Get more standard menu items in

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 use gpui::*;
+use serde::Deserialize;
 
 const APP_NAME: &str = "Br0wse";
+
+#[derive(Clone, PartialEq, Deserialize)]
+pub struct OpenBrowser {
+    pub url: String,
+}
+
+impl_actions!(br0wse, [OpenBrowser]);
+
+pub(crate) mod editor {
+    pub(crate) mod actions {
+        gpui::actions!(editor, [Copy, Cut, Paste, SelectAll, Redo, Undo,]);
+    }
+}
 
 struct HelloWorld {
     text: SharedString,
@@ -29,6 +43,27 @@ fn basic_setup(cx: &mut AppContext) {
     cx.on_action(close_active_item);
     cx.bind_keys([KeyBinding::new("cmd-w", CloseActiveItem, None)]);
     cx.on_action(about);
+    cx.on_action(|action: &OpenBrowser, cx| cx.open_url(&action.url));
+    cx.on_action(|_: &Minimize, cx| {
+        cx.defer(|cx| {
+            if let Some(window) = cx.active_window() {
+                _ = window.update(cx, |_, cx| {
+                    cx.minimize_window();
+                    true
+                });
+            }
+        });
+    });
+    cx.on_action(|_: &Zoom, cx| {
+        cx.defer(|cx| {
+            if let Some(window) = cx.active_window() {
+                _ = window.update(cx, |_, cx| {
+                    cx.zoom_window();
+                    true
+                });
+            }
+        });
+    });
 
     // Add menu items
     cx.set_menus(vec![
@@ -48,6 +83,39 @@ fn basic_setup(cx: &mut AppContext) {
                 MenuItem::action("Close Window", CloseActiveItem),
             ],
         },
+        Menu {
+            name: "Edit",
+            items: vec![
+                MenuItem::os_action("Undo", editor::actions::Undo, OsAction::Undo),
+                MenuItem::os_action("Redo", editor::actions::Redo, OsAction::Redo),
+                MenuItem::separator(),
+                MenuItem::os_action("Cut", editor::actions::Cut, OsAction::Cut),
+                MenuItem::os_action("Copy", editor::actions::Copy, OsAction::Copy),
+                MenuItem::os_action("Paste", editor::actions::Paste, OsAction::Paste),
+                MenuItem::os_action(
+                    "Select All",
+                    editor::actions::SelectAll,
+                    OsAction::SelectAll,
+                ),
+            ],
+        },
+        Menu {
+            name: "Window",
+            items: vec![
+                MenuItem::action("Minimize", Minimize),
+                MenuItem::action("Zoom", Zoom),
+                MenuItem::separator(),
+            ],
+        },
+        Menu {
+            name: "Help",
+            items: vec![MenuItem::action(
+                "Documentation",
+                OpenBrowser {
+                    url: "https://github.com/monkeydom/br0wse".into(),
+                },
+            )],
+        },
     ]);
 }
 
@@ -61,7 +129,10 @@ fn main() {
 }
 
 // Associate actions using the `actions!` macro (or `impl_actions!` macro)
-actions!(br0wse, [About, NewWindow, CloseActiveItem, Quit]);
+actions!(
+    br0wse,
+    [About, NewWindow, CloseActiveItem, Quit, Zoom, Minimize]
+);
 
 // Define the quit function that is registered with the AppContext
 fn quit(_: &Quit, cx: &mut AppContext) {
